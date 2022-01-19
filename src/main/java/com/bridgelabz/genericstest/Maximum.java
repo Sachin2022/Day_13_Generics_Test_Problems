6cbac78aff0fed41b8a3b83ddc35d0e003887759
Textspot <sachinkatarki1995@gmail.com>
2022-01-19 19:10:49 +0530

    Added:Refactor to creat Generic Class in Three Variables

--- src/main/java/com/bridgelabz/genericstest/Maximum.java
+++ src/main/java/com/bridgelabz/genericstest/Maximum.java
@@ -1,16 +1,29 @@
 package com.bridgelabz.genericstest;
 
-public class Maximum {
+public class Maximum<E extends Comparable> {
+	E value1;
+	E value2;
+	E value3;
+
+	Maximum(E value1, E value2, E value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
+
+	public E getmaximum() {
+		E result = getmaximum(value1, value2, value3);
+		return result;
+	}
 
 	public static <E extends Comparable> E getmaximum(E value1, E value2, E value3) {
 		E max = value1;
-		if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+		if (value2.compareTo(max) > 0 && value2.compareTo(value3) > 0)
 			max = value2;
-		else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+		else if (value3.compareTo(max) > 0)
 			max = value3;
 
 		return max;
-
 	}
 
 }
