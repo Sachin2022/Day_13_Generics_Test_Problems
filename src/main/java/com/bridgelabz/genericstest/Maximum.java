f302b77577e73a4a90f50ba4db4c06c4a65fec81
Textspot <sachinkatarki1995@gmail.com>
2022-01-20 13:45:55 +0530

    Added:Programme to Extend Max Method more than Three Method

--- src/main/java/com/bridgelabz/genericstest/Maximum.java
+++ src/main/java/com/bridgelabz/genericstest/Maximum.java
@@ -1,29 +1,28 @@
 package com.bridgelabz.genericstest;
 
 public class Maximum<E extends Comparable> {
-	E value1;
-	E value2;
-	E value3;
+	E[] inputarray;
 
-	Maximum(E value1, E value2, E value3) {
-		this.value1 = value1;
-		this.value2 = value2;
-		this.value3 = value3;
+	Maximum(E[] inputarray) {
+		this.inputarray = inputarray;
+
 	}
 
 	public E getmaximum() {
-		E result = getmaximum(value1, value2, value3);
+		E result = getmaximum(inputarray);
 		return result;
 	}
 
-	public static <E extends Comparable> E getmaximum(E value1, E value2, E value3) {
-		E max = value1;
-		if (value2.compareTo(max) > 0 && value2.compareTo(value3) > 0)
-			max = value2;
-		else if (value3.compareTo(max) > 0)
-			max = value3;
+	public static <E extends Comparable> E getmaximum(E[] inputarray) {
+		E max = inputarray[0];
+		for (E element : inputarray) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
 
 		return max;
+
 	}
 
 }
